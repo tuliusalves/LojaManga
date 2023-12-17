022ad70c9133d8376222f502343234e4b3cf2f24
Túlius <tulius_sr@outlook.com>
2023-12-16 21:08:51 -0300

    Adicionando atributos e o método de adicionar manga

--- src/entities/Manga.java
+++ src/entities/Manga.java
@@ -1,16 +1,22 @@
 package entities;
 
 public class Manga extends Produto{
+
     public Manga() {
+        super();
     }
     public Manga(String nome, Integer id, double preco){
         this.setNome(nome);
         this.setId(id);
         this.setPreco(preco);
+        this.setEstoque(+1);
     }
 
     @Override
     public String toString() {
-        return "Manga{}";
+
+        return "nome: "+ getNome() +" Id:"+getId()+" Preço R$:"+getPreco()+ " estoque: "+getEstoque()+"\n";
+
     }
+
 }
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,9 +1,22 @@
 package entities;
 
+import java.util.Objects;
+
 public abstract class Produto {
     private String nome;
     private Integer id;
     private double preco;
+    private int estoque;
+
+
+    public Produto(){}
+
+    public Produto(String nome, Integer id, double preco) {
+        this.nome = nome;
+        this.id = id;
+        this.preco = preco;
+
+    }
 
     public String getNome() {
         return nome;
@@ -29,12 +42,28 @@ public abstract class Produto {
         this.preco = preco;
     }
 
-    @Override
-    public String toString() {
-        return "Produto{" +
-                "nome='" + nome + '\'' +
-                ", id=" + id +
-                ", preco=" + preco +
-                '}';
+    public int getEstoque() {
+        return estoque;
     }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
 }
